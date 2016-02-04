ed63ce7d1cb34071a3b5f47880589aad322cb4e3
Igor Stajic <istajic@aortasoftware.com>
2016-02-04 18:29:48 +0100

    Added email notification.

--- merchant-web-app/routes/finalize-order.js
+++ merchant-web-app/routes/finalize-order.js
@@ -2,38 +2,58 @@ var Policy = require('mongoose').model('Policy');
 var Order = require('mongoose').model('Order');
 
 var env = require('../config/environment');
+var nodemailer = require('nodemailer');
 
 module.exports = app => {
   app.route('/finalize-order')
     .post((req, res, next) => {
       // Return redirection url.
       console.log(req.body);
-      var responseUrl = env.failedUrl;
-      if (req.body.finalizeRequest.result === 'success') {
-        // TODO: Send email.
-        Order.findOne({'orderId': req.body.finalizeRequest.merchantOrderId}, 'policy', (error, order) => {
+      const orderStatus = req.body.finalizeRequest.result === 'success';
+      var responseUrl = (orderStatus) ? env.successUrl : env.failedUrl;
+
+      Order.findOne({
+        'orderId': req.body.finalizeRequest.merchantOrderId
+      }, 'policy', (error, order) => {
+        if (error) {
+          return res.json({
+            'error': error
+          });
+        }
+        Policy.findOne({
+          '_id': order.policy
+        }, 'persons', (error, policy) => {
           if (error) {
             return res.json({
               'error': error
             });
           }
-          Policy.findOne({'_id': order.policy}, 'persons', (error, policy) => {
-            if (error) {
-              return res.json({
-                'error': error
-              });
+          var notificationEmailAddress = '';
+          policy.persons.forEach(person => {
+            if (person.email) {
+              notificationEmailAddress = person.email;
             }
-            var notificationEmailAddress = '';
-            policy.persons.forEach(person => {
-              if (person.email) {
-                notificationEmailAddress = person.email;
-              }
-            });
-            console.log("Sending email to:", notificationEmailAddress);
+          });
+          var transporter = nodemailer.createTransport('smtps://sep2015development%40gmail.com:vuInoGoW05jT@smtp.gmail.com');
+
+          var emailBody = (orderStatus) ? 'Your order was successful.' : 'There was an error processing your order.';
+          var mailOptions = {
+            from: 'myinsurance <noreply@myinsurance.com>', // sender address
+            to: 'igorstajic273@gmail.com', // list of receivers
+            subject: 'Insurance order reciept', // Subject line
+            html: emailBody // html body
+          };
+          // send mail with defined transport object
+          transporter.sendMail(mailOptions, function(error, info) {
+            if (error) {
+              return console.log(error);
+            }
+            console.log('Message sent: ' + info.response);
           });
         });
-        responseUrl = env.successUrl;
-      }
+      });
+      responseUrl = env.successUrl;
+
       res.json({
         'url': responseUrl
       });
