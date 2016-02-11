a60ccfb33da3a53a6dae1d9ed669413edd971cb4
Igor Stajic <istajic@aortasoftware.com>
2016-02-11 22:24:10 +0100

    Added check for repeating transaction

--- acquirer-web-app/routes/submit-payment.js
+++ acquirer-web-app/routes/submit-payment.js
@@ -31,68 +31,73 @@ module.exports = app => {
       'paymentId': req.params.paymentId
     }, (error, payment) => {
       if (error || payment === null) {
-        return res.json({
-          'error': {
-            'details': 'Requested payment does not exist.'
-          }
-        });
+        return handleError(res, payment.errorUrl, 'Invalid payment id.');
       }
-      var newTransaction = new Transaction({
-        'acquirerTimestamp': new Date().getTime(),
+      Transaction.findOne({
         'payment': payment._id
-      });
-      newTransaction.save((error, transaction) => {
-        if (error || transaction === null) {
-          return handleError(res, payment.errorUrl, error);
-        }
-        // Payment request to acquirer app.
-        r.post({
-          'url': env.acquirerUrl,
-          'json': true,
-          'rejectUnauthorized': false,
-          'body': {
-            'paymentRequest': {
-              'pan': req.body.cardInfo.pan,
-              'securityCode': req.body.cardInfo.securityCode,
-              'cardHolderName': req.body.cardInfo.cardHolderName,
-              'cardExpiryDate': req.body.cardInfo.cardExpiryDate,
-              'transactionAmount': payment.amount,
-              'acquirerOrderId': transaction.acquirerOrderId,
-              'acquirerTimestamp': transaction.acquirerTimestamp
-            }
-          }
-        }, (error, httpResponse, body) => {
-          // Get redirect url from merchant-web-app.
-          if (!body) {
-            return handleError(res, payment.errorUrl, error);
-          }
-          if (body.error) {
-            return handleError(res, payment.errorUrl, error);
-          }
-          r.post({
-            'url': env.finalizeUrl,
-            'json': true,
-            'rejectUnauthorized': false,
-            'body': {
-              'finalizeRequest': {
-                'result': body.paymentResponse.status,
-                'paymentId': req.params.paymentId,
-                'merchantOrderId': payment.merchantOrderId,
-                'acquirerOrderId': body.paymentResponse.acquirerOrderId,
-                'acquirerTimestamp': body.paymentResponse.acquirerTimestamp
-              }
-            }
-          }, (error, httpResponse, body) => {
-            if (error) {
+      }, (error, transaction) => {
+        if (error || transaction) {
+          return handleError(res, payment.errorUrl, 'Transaction in progress.');
+
+        } else {
+          var newTransaction = new Transaction({
+            'acquirerTimestamp': new Date().getTime(),
+            'payment': payment._id
+          });
+          newTransaction.save((error, transaction) => {
+            if (error || transaction === null) {
               return handleError(res, payment.errorUrl, error);
             }
-            res.json({
-              'redirect': {
-                'url': body.url
+            // Payment request to acquirer app.
+            r.post({
+              'url': env.acquirerUrl,
+              'json': true,
+              'rejectUnauthorized': false,
+              'body': {
+                'paymentRequest': {
+                  'pan': req.body.cardInfo.pan,
+                  'securityCode': req.body.cardInfo.securityCode,
+                  'cardHolderName': req.body.cardInfo.cardHolderName,
+                  'cardExpiryDate': req.body.cardInfo.cardExpiryDate,
+                  'transactionAmount': payment.amount,
+                  'acquirerOrderId': transaction.acquirerOrderId,
+                  'acquirerTimestamp': transaction.acquirerTimestamp
+                }
               }
+            }, (error, httpResponse, body) => {
+              // Get redirect url from merchant-web-app.
+              if (!body) {
+                return handleError(res, payment.errorUrl, error);
+              }
+              if (body.error) {
+                return handleError(res, payment.errorUrl, error);
+              }
+              r.post({
+                'url': env.finalizeUrl,
+                'json': true,
+                'rejectUnauthorized': false,
+                'body': {
+                  'finalizeRequest': {
+                    'result': body.paymentResponse.status,
+                    'paymentId': req.params.paymentId,
+                    'merchantOrderId': payment.merchantOrderId,
+                    'acquirerOrderId': body.paymentResponse.acquirerOrderId,
+                    'acquirerTimestamp': body.paymentResponse.acquirerTimestamp
+                  }
+                }
+              }, (error, httpResponse, body) => {
+                if (error) {
+                  return handleError(res, payment.errorUrl, error);
+                }
+                res.json({
+                  'redirect': {
+                    'url': body.url
+                  }
+                });
+              });
             });
           });
-        });
+        }
       });
     });
   });
--- issuer-app/config/environment.js
+++ issuer-app/config/environment.js
@@ -1,3 +1,5 @@
 module.exports = {
   db: 'mongodb://istajic:admin123@ds037015.mongolab.com:37015/dev1'
 };
+
+// example card numbers:  4532567110267702, 4532524613963444, 4532539030389239
